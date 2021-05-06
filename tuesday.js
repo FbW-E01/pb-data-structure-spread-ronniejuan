870049e7ccc285d21ff6f8ede29e2a51c7193a9c
ronniejuan <hofmannronnie@gmail.com>
2021-05-06 14:11:38 +0200

    i thing i had push it...

--- tuesday.js
+++ tuesday.js
@@ -8,29 +8,44 @@ const euroCountries=["Portugal","Chipre","Malta"]
 const asianCountries=["Palestina","Japan","Iran"]
 const cloneme=[...euroCountries,... asianCountries]
 
-console.log(cloneme)
+console.log("task1",cloneme)
+
+
+const arra1=["ron","bom","john"];
+const arra2 =["rolling","can","yes"];
+const clone =[...arra1, ...arra2];
+console.log("task1.1",clone)
+
 
 
 // #### 2. Copying Arrays
 // * Copy an array using the spread operator. Store the copied array in another variable. 
- const wh =[...asianCountries]
-
- console.log("task 2", wh)
+ const asianCountriesCopy =[...asianCountries]
+ asianCountries.push("Mongolia");
+ console.log("task 2", asianCountries);
  
+
 
 // #### 3. Find the Largest... 
 // Create a function to find the largest number in an array.
 
+
+const numbers =[23,54,6453,77,575,3,9876]
+const getLargestNumber =(x) =>Math.max(...x);
+console.log("task 3.0", getLargestNumber(numbers));
+
+
+
 const arrNum  = [1,3,5,76,98,108];
 const biggestNum =Math.max(...arrNum);
-console.log(biggestNum);
+console.log("Task3" ,biggestNum);
 
-function findnum (number){
-//    return  Math.max(number)
-   console.log("Task3",Math.max(...number))
-}
-findnum(arrNum)
-console.log("task3", findnum(34))
+// function findnum (number){
+// //    return  Math.max(number)
+//    console.log("Task3",Math.max(...number))
+// }
+// findnum(arrNum)
+// console.log("task3", findnum(34))
 
 
 
@@ -40,7 +55,7 @@ console.log("task3", findnum(34))
 
 const arrNumb  = [1,3,5,76,98,108];
 const biggestNumb =Math.min(... arrNum);
-console.log(biggestNumb);
+console.log("Task4" ,biggestNumb);
 
 
 
@@ -67,8 +82,8 @@ console.log(biggestNumb);
  console.log("This is Final Object",finalobj);
 
  job.role = "musician"
- console.log("We change the Job source",job)
- console.log("thi is the result of OBJ after Change the Job Source",finalobj)
+ console.log("Task5" ,"We change the Job source",job)
+ console.log("Task5.5" ,"this is the result of OBJ after Change the Job Source",finalobj)
 
 
 
